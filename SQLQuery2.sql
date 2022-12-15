9c8c8e115bc4ca656062c417d7bc0265d8c4b7a8
Yuneno <111202711+Yuneno@users.noreply.github.com>
2022-12-14 18:33:36 -0600

    actu db

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -12,15 +12,16 @@ id_prod int not null primary key identity(1,1),
 prod_name varchar(150),
 prod_price varchar(50),
 prod_date datetime default getdate(),
-/*prod_type int FOREIGN KEY (*/
+prod_type int not null,
+CONSTRAINT FK_producto_prod_type FOREIGN KEY (prod_type) REFERENCES prod_type (id_typProd)
 );
 
-create table prod_type
+/*create table prod_type
 (
 id_typProd int not null primary key identity (1,1),
 type_names varchar (50),
 
-)
+)*/
 
 
 
