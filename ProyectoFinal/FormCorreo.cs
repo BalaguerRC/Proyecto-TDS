304a695018d6f5517777630085803ce2166fa193
Liliana Castillo <202010776@itla.edu.do>
2022-12-17 08:49:53 -0400

    finalTDS

---
+++ ProyectoFinal/FormCorreo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace ProyectoFinal
+{
+    public partial class FormCorreo : Form
+    {
+        public FormCorreo()
+        {
+            InitializeComponent();
+        }
+
+        private void enviarEmail(string para, string asunto, string mensaje)
+        {
+            try
+            {
+
+                MailMessage msg = new MailMessage();
+                SmtpClient cliente = new SmtpClient("smtp.gmail.com", 587);
+                msg.From = new MailAddress("lectorexample@gmail.com", "Lector Programa");
+                msg.To.Add(para);
+                msg.Subject =asunto;
+                msg.Body = mensaje;  //txtMensaje.TexT
+                msg.IsBodyHtml = false;
+
+                //msg.SubjectEncoding = Encoding.UTF8;
+                //msg.BodyEncoding = Encoding.UTF8;
+                cliente.Credentials = new NetworkCredential("lectorexample@gmail.com", "mmxymtcgptgnxlpt");
+                cliente.EnableSsl = true;
+                cliente.Send(msg);
+                MessageBox.Show("Enviado", "Aviso Email");
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Ocurrio un error", "Aviso Email");
+            }
+        }
+
+        private void btnEnvio_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPara.Text))
+            {
+                MessageBox.Show("Escriba el destinatario", "Aviso Email");
+            }
+            else if (string.IsNullOrEmpty(txtAsunto.Text))
+            {
+                MessageBox.Show("Escriba el asunto", "Aviso Email");
+            }
+            else if (string.IsNullOrEmpty(txtMensaje.Text))
+            {
+                MessageBox.Show("Escriba el mensaje", "Aviso Email");
+            }
+            else
+            {
+                enviarEmail(txtPara.Text, txtAsunto.Text, txtMensaje.Text);
+            }
+            //enviarEmail();
+        }
+    }
+}
---
+++ ProyectoFinal/FormMenuE.cs
@@ -0,0 +1,113 @@
+﻿using DataBase.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal
+{
+    public partial class FormMenuE : Form
+    {
+        public FormMenuE()
+        {
+            InitializeComponent();
+        }
+        #region inuntil
+        private void FormMenuPrincipal_Load(object sender, EventArgs e)
+        {
+            OpenForm(new FormInicio());
+            Date();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Date();
+        }
+
+
+        private void btnCorreo_Click(object sender, EventArgs e)
+        {
+            OpenForm(new FormCorreo());
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            AlLogin();
+        }        
+        private void btnInventario_Click(object sender, EventArgs e)
+        {
+            OpenForm(new FormInventarioAdmin());
+        }
+        #endregion
+
+        private void Date()
+        {
+            toolstripfecha.Text = DateTime.Now.ToString("dddd MMMM yyyy");
+            toolStripfecha2.Text = DateTime.Now.ToString("h:mm:ss");
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            OpenForm(new FormInicio());
+        }
+
+
+       /* private void btnUsers_Click(object sender, EventArgs e)
+        {
+            OpenForm(new FormUsers());
+        }*/
+
+        #region metodos
+        private void AlLogin()
+        {
+            this.Close();
+        }
+        private void OpenForm(object formHijo)
+        {
+            if (this.panelMostrar.Controls.Count > 0)
+                this.panelMostrar.Controls.RemoveAt(0);
+            Form form = formHijo as Form;
+            form.TopLevel = false;
+            form.Dock = DockStyle.Fill;
+            this.panelMostrar.Controls.Add(form);
+            this.panelMostrar.Tag = form;
+            form.Show();
+        }
+        #endregion
+
+        private void btnInicio_Click_1(object sender, EventArgs e)
+        {
+            OpenForm(new FormInicio());
+        }
+
+        private void btnCorreo_Click_1(object sender, EventArgs e)
+        {
+            OpenForm(new FormCorreo());
+        }
+
+        private void btnInventario_Click_1(object sender, EventArgs e)
+        {
+            OpenForm(new FormInventarioAdmin());
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            Date();
+        }
+
+        private void FormMenuE_Load(object sender, EventArgs e)
+        {
+            OpenForm(new FormInicio());
+            Date();
+        }
+
+        private void btnsalir_Click_1(object sender, EventArgs e)
+        {
+            AlLogin();
+        }
+    }
+}
